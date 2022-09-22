418e5b798d8d10a35a4f521f681265f0675cae67
Pulkit Gupta <pulkit30.bsr@gmail.com>
2022-09-22 20:18:50 +0530

    support: local development (#94)

--- build/script.js
+++ build/script.js
@@ -5,6 +5,25 @@ document.addEventListener("scroll", (e) => {
     document.getElementById("navbar").classList.remove("nav-bg");
   }
 });
+
+// update stylesheets
+const getStyleSheets = () => {
+  const files =
+    "style.css/global.css/utils.css/header.css/homePage.css/aboutPage.css/schedule.css/themePage.css/faqPage.css/footer.css/team.css/sponsor.css/avatars.css/contactForm.css/animation.css".split(
+      "/"
+    );
+  var styleSheetsHTML = "";
+  files.forEach(
+    (name) =>
+      (styleSheetsHTML += ` <link rel="stylesheet" href="./styles/${name}">`)
+  );
+  console.log(styleSheetsHTML);
+  return styleSheetsHTML;
+};
+
+if (window.location.href != "https://hacknexus.me/") {
+  document.querySelector("head").innerHTML += getStyleSheets();
+}
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const imgUrlGenerator = () => {
